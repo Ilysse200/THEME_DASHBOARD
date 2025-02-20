6e02bdb8743a2a2cfb04ad087444ddcfef6e4fe9
Ilysse200 <ilysseted500@gmail.com>
2025-02-20 11:23:06 +0200

    modified the sidebar's admin menu

--- dashboard_theme/src/Dashboard/Sidebar.jsx
+++ dashboard_theme/src/Dashboard/Sidebar.jsx
@@ -15,6 +15,22 @@ import avatar_image from "../assets/avatar-1.jpg";
 //Icon for toggling the list in the sidebar for the dashboard
 import { IoIosArrowForward } from "react-icons/io";
 
+
+//Import membership icon
+import { IoPersonSharp } from "react-icons/io5";
+
+//Import helpdesk icon
+import { SiHelpdesk } from "react-icons/si";
+
+//Import of the filter icon
+import { IoFilterOutline } from "react-icons/io5";
+
+//Icon for online courses
+import { TbStack2Filled } from "react-icons/tb";
+
+//Import of the invoice icon
+import { PiInvoice } from "react-icons/pi";
+
 function Sidebar() {
     //define states for toggling within the sidebar the navigation
 
@@ -37,17 +53,36 @@ function Sidebar() {
     const showToggleStudent =() =>{
         showStudent(!toggleStudent);
     }
+    //States to show or hide the admin content
+
+    const [showAdminContent, setShowAdminContent] =useState(false);
+    const toggleAdminContent =() =>{
+        setShowAdminContent(!showAdminContent)
+    }
+
+
+    //Toggle lists of online courses
+    const[hideOnlineCourses, showOnlineCourses]=useState(false);
+    
+    const toggleOnlineCourses =() =>{
+        showOnlineCourses(!hideOnlineCourses)
+    }
 
   return (
     <div className='sidebar'>
         <p className='top-sidebarcontent'>Able</p>
         <p className='content-prefix'>Pro</p>
-        <div className='admin-menu'>
+        <div className={`admin-menu ${showAdminContent ? "open" : ""}`}>
+            <div className='filter-container'>
+            <IoFilterOutline className='filter-icon' onClick={toggleAdminContent}/>
+            </div>
             <div className='admin-info'>
             <img src={avatar_image} className='avatar-icon'/>
             <p className='admin-name'><strong>John Smith</strong><br/>Administrator</p>
             </div>
-        
+    
+            {showAdminContent &&(
+                <>
             <ul className='sidebar-list'>
                 <li className='description'><RiAccountBoxLine className='account-icon'/>My account</li>
                 <li className='description'><VscSettingsGear className='account-icon'/>Settings</li>
@@ -55,7 +90,10 @@ function Sidebar() {
                 <li className='description'><IoIosPower className='account-icon'/>Logout</li>
                 {/* <li></li> */}
             </ul>
+            </>
+            )}
         </div>
+        {/** Navigation content below */}
         <div className='navigation-content'>
             <h3 className='nav-title'>Navigation</h3>
             <ul className='nav-list'>
@@ -95,9 +133,18 @@ function Sidebar() {
             <h3 className='nav-title'>Admin Panel</h3>
 
             {/**Content for Admin Panel */}
+
             <ul className='admin-content'>
-                <li className='main-list'>Online Courses</li>
-                <li>Dashboard</li>
+                <li className='main-list' >
+                <TbStack2Filled className='online-courses'/>
+                    Online Courses
+                <IoIosArrowForward className='forward-icon' 
+                    onClick={toggleOnlineCourses}/>
+                </li>
+                {hideOnlineCourses && (
+                    <> 
+                    <ul className='online-courses-list'>
+                    <li>Dashboard</li>
                 <li className='teacher-contents'>Teacher
                 <IoIosArrowForward className='forward-icon' 
                 onClick={showToggleStudent} />
@@ -124,9 +171,22 @@ function Sidebar() {
                 <li>Pricing</li>
                 <li>Site</li>
                 <li>Setting</li>
-                <li>Membership</li>
-                <li>Helpdesk</li>
-                <li>Invoice</li>
+                    </ul>
+                    </>
+                )}
+                {/*This is where the membership contents begin*/}
+                <li className='main-list'><IoPersonSharp className='online-courses' />
+                    Membership
+                    <IoIosArrowForward className='forward-icon'/>
+                    </li>
+                <li className='main-list'><SiHelpdesk className='online-courses'/>
+                    Helpdesk
+                    <IoIosArrowForward className='forward-icon'/>
+                </li>
+                <li className='main-list'><PiInvoice className='online-courses'/>
+                    Invoice
+                <IoIosArrowForward className='forward-icon'/>
+                    </li>
             </ul>
         </div>
         </div>
